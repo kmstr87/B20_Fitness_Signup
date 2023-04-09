202e45ee03291294c4704e948c8b2e21df779bb4
In Kim <in.kim0128@gmail.com>
2023-04-09 04:23:55 -0400

    Merged mult. diff changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\leoni\Desktop\B20 A3 Project\venv
+set VIRTUAL_ENV=C:\Users\leoni\Desktop\CSCB20 A3 Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
