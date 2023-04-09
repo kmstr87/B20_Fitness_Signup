38f0dd236801718272f79e591e9eab5663af6104
Leon <leoninvancouver@gmail.com>
2023-04-09 03:58:36 -0400

    Updated styling and tutorial page

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_bcrypt import Bcrypt
+from datetime import timedelta
+import sqlite3
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+"""bcrypt = Bcrypt(app)
+app.config['SECRET_KEY'] = 'dkf3sldkjfDF23fLJ3b'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 10)"""
+
+#connect to the SQL Database
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+# Creates the User table
+sql_query = """
+    CREATE TABLE IF NOT EXISTS User 
+    (
+        username TEXT PRIMARY KEY, 
+        password TEXT,
+        email TEXT
+    )
+"""
+cur.execute(sql_query)
+
+@app.route('/')
+def home():
+        return render_template("home.html")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+        if (request.method == "GET"):
+                return render_template("login.html")
+        else:
+                if "form-login" in request.form:
+                        username = request.form['uname']
+                        password = request.form['psw']
+                        print(username)
+                        print(password)
+                        return render_template("home.html")
+                elif "form-register" in request.form:
+                        username = request.form['new_uname']
+                        password = request.form['new_psw']
+                        print(username)
+                        print(password)
+                        return render_template("login.html")
+
+@app.route('/calendar')
+def calendar():
+        return render_template("calendar.html")
+
+@app.route('/contacts')
+def contacts():
+        return render_template("contacts.html")
+
+"Extra Pages to be added"
+
+@app.route('/tutorials')
+def tutorials():
+        return render_template("tutorials.html")
+
+@app.route('/about')
+def about():
+        return render_template("about.html")
+
+        
+app.run(host='0.0.0.0', port=81, debug=True)
---
+++ static/js/login.js
@@ -0,0 +1,17 @@
+const loginSelect = document.querySelector(".pill-login");
+const registerSelect = document.querySelector(".pill-register");
+document.querySelector(".register-content").style.display = "none";
+
+loginSelect.addEventListener("click", () => {
+  registerSelect.classList.remove("active");
+  loginSelect.classList.add("active");
+  document.querySelector(".register-content").style.display = "none";
+  document.querySelector(".login-content").style.display = "block";
+});
+
+registerSelect.addEventListener("click", () => {
+  loginSelect.classList.remove("active");
+  registerSelect.classList.add("active");
+  document.querySelector(".register-content").style.display = "block";
+  document.querySelector(".login-content").style.display = "none";
+});
